11a55f4c4576d32d88cdb91f006cf525effd5deb
yuvante <kinga-stasiak@outlook.com>
2026-08-08 23:56:42 +0200

    Add RE2 background, logo, icon and UI styling

--- src/Window.java
+++ src/Window.java
@@ -6,11 +6,19 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+import java.awt.*;
+import javafx.geometry.Insets;
+import javafx.scene.text.Font;
 
 public class Window extends Application {
 
@@ -25,28 +33,85 @@ public class Window extends Application {
 
         ScrollPane scrollPane = new ScrollPane(achievementsBox);
         scrollPane.setFitToWidth(true);
+        scrollPane.setPrefWidth(400);
+        scrollPane.setStyle(
+                "-fx-background-color: transparent;" +
+                        "-fx-background: transparent;"
+        );
+
+
+        achievementsBox.setStyle("-fx-background-color: transparent;");
 
         BorderPane root = new BorderPane();
-        root.setCenter(scrollPane);
 
         IntegerProperty completed = new SimpleIntegerProperty(0);
 
         ProgressBar progressBar = new ProgressBar(0);
         progressBar.setMaxWidth(Double.MAX_VALUE);
+        progressBar.setPrefWidth(1550);
+        progressBar.setPrefHeight(35);
+        progressBar.setStyle(
+                "-fx-accent: #b00020;"
+        );
 
-        Label progressLabel = new Label("0 / " + TOTAL_ACHIEVEMENTS);
+        Label progressLabel = new Label("0/" + TOTAL_ACHIEVEMENTS);
+        Font requiemFont = Font.loadFont(getClass().getResourceAsStream("/font.ttf"), 20);
+        progressLabel.setFont(requiemFont);
+        progressLabel.getStyleClass().add("progress-label");
 
         StackPane progressPane = new StackPane(
                 progressBar,
                 progressLabel
         );
+        progressPane.setPrefHeight(30);
+        progressPane.setMaxHeight(30);
+        progressBar.setMaxWidth(Double.MAX_VALUE);
+        progressBar.setMaxHeight(Double.MAX_VALUE);
 
         root.setBottom(progressPane);
+        root.setStyle("-fx-background-color: transparent;");
+
+        Media background = new Media(getClass().getResource("/re2.mp4").toExternalForm());
+        MediaPlayer  mediaPlayer = new MediaPlayer(background);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+        MediaView  mediaView = new MediaView(mediaPlayer);
+
+        StackPane backgroundPane = new StackPane();
+
+        backgroundPane.getChildren().addAll(
+                mediaView,
+                root
+        );
+
+        backgroundPane.setStyle("-fx-background-color: black;");
+        mediaView.fitWidthProperty().bind(backgroundPane.widthProperty());
+        mediaView.fitHeightProperty().bind(backgroundPane.heightProperty());
+        mediaView.setPreserveRatio(true);
+
+        Image icon = new Image(getClass().getResource("/re2 icon.png").toExternalForm());
+        stage.getIcons().add(icon);
+
+        Image logo = new Image(getClass().getResource("/re2 logo.png").toExternalForm());
+        ImageView logoView = new ImageView(logo);
+        logoView.setFitWidth(450);
+        logoView.setPreserveRatio(true);
+
+        VBox leftPanel = new VBox();
+        leftPanel.getChildren().addAll(
+                logoView,
+                scrollPane
+        );
+        root.setLeft(leftPanel);
+        leftPanel.setPrefWidth(450);
+        leftPanel.setSpacing(20);
+        leftPanel.setPadding(new Insets(10));
+
+        //will later add search bar, filtering and sorting (category and difficulty)
 
         for (Achievement achievement : repository.getAllAchievements()) {
 
             CheckBox checkBox = new CheckBox();
-
             Label achievementLabel = new Label(
                     achievement.getId()
                             + " | "
@@ -58,6 +123,7 @@ public class Window extends Application {
                             + " | "
                             + achievement.getHint()
             );
+            achievementLabel.setFont(Font.font("Lucida Sans Typewriter", 13));
 
             HBox achievementRow = new HBox(
                     6,
@@ -66,6 +132,7 @@ public class Window extends Application {
             );
 
             achievementsBox.getChildren().add(achievementRow);
+
 
             checkBox.setOnAction(event -> {
 
@@ -85,10 +152,13 @@ public class Window extends Application {
             });
         }
 
-        Scene scene = new Scene(root, 700, 700);
-
+        Scene scene = new Scene(backgroundPane, 1550, 800);
+        stage.setResizable(false);
+        scene.getStylesheets().add(
+                getClass().getResource("/style.css").toExternalForm());
         stage.setTitle("Resident Evil 2 | Achievement Tracker");
         stage.setScene(scene);
         stage.show();
     }
+
 }
